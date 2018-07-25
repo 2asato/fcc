832d39c58165e13363073040a0e0219464eaa001
aaronsato <2asato@gmail.com>
2018-07-24 18:08:07 -0700

    put it on the board

---
+++ basic_algorithm_scripting/confirm_ending.js
@@ -0,0 +1,18 @@
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  let targetLength = target.length;
+  // console.log(targetLength);
+  let endingToTarget = str.substr(str.length - targetLength);
+  // console.log(endingToTarget);
+  if (target === endingToTarget){
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+  return str;
+}
+
+confirmEnding("Abstraction", "action");
