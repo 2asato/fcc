9dfb7d700b15a0e1c15adf949b5639c7125703ce
aaronsato <2asato@gmail.com>
2018-07-25 13:04:21 -0700

    got it with some searching

---
+++ basic_algorithm_scripting/repeat_a_string.js
@@ -0,0 +1,33 @@
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  let repeated = "";
+  if (num < 0) {
+    console.log("");
+    return "";
+  } else if (num === 1) {
+    console.log(str);
+    return str;
+  } else {
+    console.log(str + repeatStringNumTimes(str, num - 1));
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
+
+repeatStringNumTimes("abc", 3);
+
+
+// repeat method
+// function repeatStringNumTimes(str, num) {
+//   // repeat after me
+//   let repeated = "";
+//   if (num > 0) {
+//     repeated = str.repeat(num);
+//     console.log(repeated);
+//     return repeated;
+//   } else {
+//     return "";
+//   }
+//   return str;
+// }
+//
+// repeatStringNumTimes("abc", 3);
