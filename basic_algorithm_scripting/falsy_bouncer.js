8260ec90bef1a5d862a770c1ae878da22d1a5f8f
aaronsato <2asato@gmail.com>
2018-07-30 18:27:47 -0700

    solved

--- basic_algorithm_scripting/falsy_bouncer.js
+++ basic_algorithm_scripting/falsy_bouncer.js
@@ -1,15 +1,6 @@
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
-  let minors = [];
-  for (let i = 0; i < arr.length; i++) {
-    // console.log(arr[i]);
-    // console.log(Boolean(arr[i]));
-    if (Boolean(arr[i]) === false) {
-      minors = arr.splice(i, 1);
-    }
-  }
-  console.log(minors);
-  return arr;
+  return arr.filter(Boolean);
 }
 
 bouncer([7, "ate", "", false, 9]);
---
+++ basic_algorithm_scripting/where_do_i_belong.js
@@ -0,0 +1,16 @@
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  let pushed = arr.push(num);
+  // console.log(pushed);
+  console.log(arr);
+  let sorted = arr.sort(function(a, b){return a - b});
+  console.log(sorted);
+  let indexNum= arr.indexOf(num);
+  console.log(indexNum);
+  return indexNum;
+}
+
+// getIndexToIns([60, 40], 50);
+// getIndexToIns([10, 20, 30, 40, 50], 35);
+// getIndexToIns([5, 3, 20, 3], 5)
+getIndexToIns([2, 5, 10], 15);
