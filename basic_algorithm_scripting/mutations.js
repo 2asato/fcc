2c39c9110d636d2fd1bd0ba74305cac543c84223
aaronsato <2asato@gmail.com>
2018-08-01 20:17:33 -0700

    got it kinda get it

---
+++ basic_algorithm_scripting/mutations.js
@@ -0,0 +1,14 @@
+function mutation(arr) {
+  arr[0] = arr[0].toLowerCase();
+  console.log(arr[0]);
+  arr[1] = arr[1].toLowerCase().split("");
+  console.log(arr[1]);
+  for (let i = 0; i < arr[1].length; i++) {
+    if (arr[0].indexOf(arr[1][i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
