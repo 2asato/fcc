ac85879309bb9ef3101a7b975d3b92336c705bc1
aaronsato <2asato@gmail.com>
2018-08-03 09:02:50 -0700

    added comments

--- functional_programming/refactor_global_variables.js
+++ functional_programming/refactor_global_variables.js
@@ -6,7 +6,7 @@ var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Movi
 
 // Add your code below this line
 function add (arr, bookName) {
-
+// .concat() works like .push() but non-mutating
   return arr.concat(bookName);
   // Add your code above this line
 }
@@ -17,7 +17,9 @@ function add (arr, bookName) {
 // Add your code below this line
 function remove (arr, bookName) {
   if (arr.indexOf(bookName) >= 0) {
+    // makes copy of arr to mutate allowing arr to remain the same
     let newArr = [...arr];
+    // .splice() removes from newArr (index of what to remove, # of elements to remove)
     newArr.splice(newArr.indexOf(bookName), 1);
     return newArr;
     // return arr.slice(0, arr.indexOf(bookName) + 1)
@@ -26,8 +28,9 @@ function remove (arr, bookName) {
     }
 }
 
-// var newBookList = add(bookList, 'A Brief History of Time');
+var newBookList = add(bookList, 'A Brief History of Time');
 var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+// adds 'A Brief History...' and removes 'On The Elec...'
 var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
 
 console.log(bookList);
