4da47b7f53ea7ed77a95ebe9253eae6e0cebfac6
aaronsato <2asato@gmail.com>
2018-08-16 12:34:29 -0700

    solved with foreach method

--- functional_programming/implement_filter_method_on_prototype.js
+++ functional_programming/implement_filter_method_on_prototype.js
@@ -4,11 +4,16 @@ var s = [23, 65, 98, 5];
 Array.prototype.myFilter = function(callback){
   var newArray = [];
   // Add your code below this line
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] % 2 === 1)
-    newArray.push(s[i]);
-  }
+  // for loop
+  // for (let i = 0; i < s.length; i++) {
+  //   if (s[i] % 2 === 1)
+  //   newArray.push(s[i]);
+  // }
+  // forEach()
+  s.forEach(function(item) {
+    if (callback(item)) newArray.push(item)
   // Add your code above this line
+});
   console.log(newArray);
   return newArray;
 
