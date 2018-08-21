83b19bc808ce357b3e1545ab0132641592636f14
aaronsato <2asato@gmail.com>
2018-08-21 10:28:16 -0700

    sliced it not spliced it

--- functional_programming/slice_instead_of_splice.js
+++ functional_programming/slice_instead_of_splice.js
@@ -1,7 +1,9 @@
 function nonMutatingSplice(cities) {
   // Add your code below this line
-  return cities.splice(3);
-
+  // return cities.splice(3);
+  let sliced = cities.slice(0,3);
+  // console.log(sliced);
+  return cities;
   // Add your code above this line
 }
 var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
