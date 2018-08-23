2370dc3ef9d2afe539e41c8a9bdfab23b73e4394
aaronsato <2asato@gmail.com>
2018-08-23 09:39:55 -0700

    summed it all up

---
+++ intermediate_algorithm_scripting/sum_all_numbers_in_range.js
@@ -0,0 +1,28 @@
+function sumAll(arr) {
+  // sort array in ascending order
+  let sorted = arr.sort(function(a, b) {
+    return a - b;
+    return sorted;
+  })
+  // console.log(sorted
+  // created variable for lowest number which is the first number in the array
+  let low = sorted[0];
+  console.log(low);
+  // created variable for highest number which is the last number in array
+  let high = sorted[sorted.length-1];
+  console.log(high);
+  // created new array to store all numbers
+  let digitArray = [];
+  // added numbers to new array while they are higher than low and lower than or equal to high
+  while (low <= high) {
+    digitArray.push(low++);
+  }
+  console.log(digitArray);
+  // get total of all numbers in array
+  let totalSum = digitArray.reduce(function(a, b) {
+    return a + b;
+  })
+  console.log(totalSum);
+}
+
+sumAll([5, 10]);
