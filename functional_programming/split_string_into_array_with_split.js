1cbb04fe7358f1feb1c70af37b658abae1461d9f
aaronsato <2asato@gmail.com>
2018-08-22 22:08:39 -0700

    split it up

---
+++ functional_programming/split_string_into_array_with_split.js
@@ -0,0 +1,7 @@
+function splitify(str) {
+  // Add your code below this line
+  let splited = str.split(/\s|\W/);
+  console.log(splited);
+  // Add your code above this line
+}
+splitify("Hello World,I-am code");
