0592e8455cd580f3efd18280d9d070c94dd606c1
aaronsato <2asato@gmail.com>
2018-08-23 18:16:05 -0700

    solved with for loop easier to understand

--- intermediate_algorithm_scripting/diff_two_arrays.js
+++ intermediate_algorithm_scripting/diff_two_arrays.js
@@ -1,10 +1,22 @@
 function diffArray(arr1, arr2) {
 
   // Same, same; but different.
-  let newArr = arr1.concat(arr2);
+  // let newArr = arr1.concat(arr2);
+  // console.log(newArr);
+  // return newArr.filter(x => arr1.indexOf(x) === -1 || arr2.indexOf(x) === -1
+  // )
+  let newArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if ((arr2.indexOf(arr1[i]))=== -1) {
+      newArr.push(arr1[i])
+    }
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if ((arr1.indexOf(arr2[j]))=== -1) {
+      newArr.push(arr2[j])
+    }
+  }
   console.log(newArr);
-  return newArr.filter(x => arr1.indexOf(x) === -1 || arr2.indexOf(x) === -1
-  )
 }
 
 
