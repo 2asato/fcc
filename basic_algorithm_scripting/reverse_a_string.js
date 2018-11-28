90cdbe0ad986afae5147a9e617be8ea0c2294e84
aaronsato <2asato@gmail.com>
2018-11-28 10:52:34 -0800

    100 days redo

--- basic_algorithm_scripting/reverse_a_string.js
+++ basic_algorithm_scripting/reverse_a_string.js
@@ -1,16 +1,5 @@
 function reverseString(str) {
-  // split string at each letter
-  let splitStr = str.split("");
-  // console.log(splitStr);
-  // reverse split string
-  let reversedSplit = splitStr.reverse();
-  // console.log(reversedSplit);
-  // join split strings into one string
-  let reversedString = reversedSplit.join("");
-  // console.log(reversedString);
-  return reversedString;
-
+  return str.split('').reverse().join('');
 }
+
 console.log(reverseString("hello"));
-console.log(reverseString("Howdy"));
-console.log(reverseString("Greetings from Earth"));;
