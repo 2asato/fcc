de0f3e8063d027a1f1e2c9b759d1cab7641cc37a
aaronsato <2asato@gmail.com>
2018-11-29 11:25:16 -0800

    100days redo

--- basic_algorithm_scripting/repeat_a_string.js
+++ basic_algorithm_scripting/repeat_a_string.js
@@ -1,19 +1,65 @@
 function repeatStringNumTimes(str, num) {
   // repeat after me
-  let repeated = "";
-  if (num < 0) {
-    console.log("");
-    return "";
-  } else if (num === 1) {
-    console.log(str);
-    return str;
-  } else {
-    console.log(str + repeatStringNumTimes(str, num - 1));
-    return str + repeatStringNumTimes(str, num - 1);
+  let strArr = [];
+  for (let i = 0; i < num; i++) {
+    strArr.push(str)
   }
+  // return strArr;
+
+  console.log(strArr.join(""));
 }
 
 repeatStringNumTimes("abc", 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function repeatStringNumTimes(str, num) {
+//   // repeat after me
+//   let repeated = "";
+//   if (num < 0) {
+//     console.log("");
+//     return "";
+//   } else if (num === 1) {
+//     console.log(str);
+//     return str;
+//   } else {
+//     console.log(str + repeatStringNumTimes(str, num - 1));
+//     return str + repeatStringNumTimes(str, num - 1);
+//   }
+// }
+//
+// repeatStringNumTimes("abc", 3);
 
 
 // repeat method
