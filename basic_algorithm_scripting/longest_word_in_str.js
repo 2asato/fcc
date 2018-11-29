6564690658dea481c43f4c5af0373a935997358e
aaronsato <2asato@gmail.com>
2018-11-29 10:24:30 -0800

    100days redo

--- basic_algorithm_scripting/longest_word_in_str.js
+++ basic_algorithm_scripting/longest_word_in_str.js
@@ -1,19 +1,67 @@
 function findLongestWordLength(str) {
-  let strLength = [];
-  let maxLength = 0;
-  let splitStr = str.split(" ");
-  // console.log(splitStr);
-  for (let i = 0; i < splitStr.length; i++) {
-    strLength.push(splitStr[i].length);
-    // console.log(strLength);
-    maxLength = Math.max(...strLength);
-    // console.log(maxLength);
-  }
-  console.log(maxLength);
-  return maxLength;
+  // seperate str into array of seperate strings
+  let seperatedStrings = str.split(" ");
+  // using .map get the lengths of each string
+  let strLengths = seperatedStrings.map(strings => strings.length);
+  // finds the highes number of strLengths
+  return Math.max(...strLengths);
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findLongestWordLength(str) {
+//   let strLength = [];
+//   let maxLength = 0;
+//   let splitStr = str.split(" ");
+//   // console.log(splitStr);
+//   for (let i = 0; i < splitStr.length; i++) {
+//     strLength.push(splitStr[i].length);
+//     // console.log(strLength);
+//     maxLength = Math.max(...strLength);
+//     // console.log(maxLength);
+//   }
+//   console.log(maxLength);
+//   return maxLength;
+// }
+//
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
 
 
 
