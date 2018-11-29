ef6b367dcac1dd0123d2b83ee6894b7c3f8fe9d2
aaronsato <2asato@gmail.com>
2018-11-29 10:49:24 -0800

    100days redo

--- basic_algorithm_scripting/largest_num_in_array.js
+++ basic_algorithm_scripting/largest_num_in_array.js
@@ -1,16 +1,63 @@
 function largestOfFour(arr) {
   // You can do this!
-  let maxArr = [];
-  let maxNum = 0;
-  for (let i = 0; i < arr.length; i++){
-    // console.log(arr[i]);
-    maxNum = Math.max(...arr[i]);
-    // console.log(maxNum);
-    maxArr.push(maxNum);
-    // console.log(maxArr);
-  }
-  console.log(maxArr);
-  return maxArr;
+  let maxSubArr = arr.map(subArr => Math.max(...subArr));
+  // let largest = []
+  // largest.push(Math.max(...maxSubArr));
+  console.log(maxSubArr);
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function largestOfFour(arr) {
+//   // You can do this!
+//   let maxArr = [];
+//   let maxNum = 0;
+//   for (let i = 0; i < arr.length; i++){
+//     // console.log(arr[i]);
+//     maxNum = Math.max(...arr[i]);
+//     // console.log(maxNum);
+//     maxArr.push(maxNum);
+//     // console.log(maxArr);
+//   }
+//   console.log(maxArr);
+//   return maxArr;
+// }
+//
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
