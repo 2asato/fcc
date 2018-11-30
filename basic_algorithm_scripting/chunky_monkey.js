633e35589d3514f87ef58e4884c84a8c2e9b4ded
aaronsato <2asato@gmail.com>
2018-11-29 22:54:29 -0800

    100days redo

--- basic_algorithm_scripting/chunky_monkey.js
+++ basic_algorithm_scripting/chunky_monkey.js
@@ -1,12 +1,56 @@
 function chunkArrayInGroups(arr, size) {
   // Break it up.
-  let chunkyMonkey = [];
-  for ( i = 0; i < arr.length; i+=size) {
-    chunkyMonkey.push(arr.slice(i, i + size));
+  chunky = [];
+  for (let i = 0; i < arr.length; i+=size) {
+    chunky.push(arr.slice(i, i + size))
   }
-  console.log(chunkyMonkey);
-  return chunkyMonkey;
+  console.log(chunky);
 }
 
-// chunkArrayInGroups(["a", "b", "c", "d"], 2);
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function chunkArrayInGroups(arr, size) {
+//   // Break it up.
+//   let chunkyMonkey = [];
+//   for ( i = 0; i < arr.length; i+=size) {
+//     chunkyMonkey.push(arr.slice(i, i + size));
+//   }
+//   console.log(chunkyMonkey);
+//   return chunkyMonkey;
+// }
+//
+// // chunkArrayInGroups(["a", "b", "c", "d"], 2);
+// chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
