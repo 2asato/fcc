1e48088fca62ffdfcb6d59e87e53384ce3629ba7
aaronsato <2asato@gmail.com>
2018-11-29 16:27:03 -0800

    100days redo

--- basic_algorithm_scripting/slice_and_spice.js
+++ basic_algorithm_scripting/slice_and_spice.js
@@ -1,13 +1,62 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
-  let jointArr = [];
-  jointArr.splice(0, 0,...arr2);
-  console.log(jointArr);
-  jointArr.splice(n, 0,...arr1);
-  console.log(jointArr);
-  console.log(arr1);
-  console.log(arr2);
-  return jointArr;
+  // create new array with contents of arr2
+  let sliced2 = arr2.slice();
+  // console.log(sliced2);
+  // add each element off arr1 into sliced2
+  for (let i = 0; i < arr1.length; i++) {
+    sliced2.splice(n, 0, arr1[i])
+    // increases n, changing the index place of element being added
+    n++;
+    // console.log(sliced2);
+  }
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function frankenSplice(arr1, arr2, n) {
+//   // It's alive. It's alive!
+//   let jointArr = [];
+//   jointArr.splice(0, 0,...arr2);
+//   console.log(jointArr);
+//   jointArr.splice(n, 0,...arr1);
+//   console.log(jointArr);
+//   console.log(arr1);
+//   console.log(arr2);
+//   return jointArr;
+// }
+//
+// frankenSplice([1, 2, 3], [4, 5, 6], 1);
--- basic_algorithm_scripting/title_case_a_sentence.js
+++ basic_algorithm_scripting/title_case_a_sentence.js
@@ -1,16 +1,60 @@
 function titleCase(str) {
-  let splitStr = str.split(" ");
-  let upperCased = [];
-  let newStr = "";
-  // console.log(splitStr);
-  for (let i = 0; i < splitStr.length; i++) {
-    // console.log(splitStr[i]);
-    upperCased.push(splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1).toLowerCase());
-    newStr = upperCased.toString().replace(/,/g, ' ');
-    console.log(newStr);
-  }
-  return newStr;
+  strArr = str.split(" ");
+  console.log(strArr);
+  capt = strArr.map(word => word[0].toUpperCase() + word.slice(1).toLowerCase());
+  console.log(capt);
+  finalStr = capt.join(" ");
+  console.log(finalStr);
 }
 
 titleCase("I'm a little tea pot");
-titleCase("sHoRt AnD sToUt");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function titleCase(str) {
+//   let splitStr = str.split(" ");
+//   let upperCased = [];
+//   let newStr = "";
+//   // console.log(splitStr);
+//   for (let i = 0; i < splitStr.length; i++) {
+//     // console.log(splitStr[i]);
+//     upperCased.push(splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1).toLowerCase());
+//     newStr = upperCased.toString().replace(/,/g, ' ');
+//     console.log(newStr);
+//   }
+//   return newStr;
+// }
+//
+// titleCase("I'm a little tea pot");
+// titleCase("sHoRt AnD sToUt");
