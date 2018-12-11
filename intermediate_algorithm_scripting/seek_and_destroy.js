56ab3d837d40fd880ab429f8f1c37c882edcae31
aaronsato <2asato@gmail.com>
2018-12-11 15:47:30 -0800

    100days redo

---
+++ intermediate_algorithm_scripting/seek_and_destroy.js
@@ -0,0 +1,7 @@
+function destroyer(arr) {
+  // Remove all the values
+  console.log(arguments[0]);
+  return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
--- intermediate_algorithm_scripting/sum_all_numbers_in_range.js
+++ intermediate_algorithm_scripting/sum_all_numbers_in_range.js
@@ -1,28 +1,85 @@
 function sumAll(arr) {
-  // sort array in ascending order
   let sorted = arr.sort(function(a, b) {
     return a - b;
-    return sorted;
+    return sorted
   })
-  // console.log(sorted
-  // created variable for lowest number which is the first number in the array
-  let low = sorted[0];
-  console.log(low);
-  // created variable for highest number which is the last number in array
-  let high = sorted[sorted.length-1];
-  console.log(high);
-  // created new array to store all numbers
-  let digitArray = [];
-  // added numbers to new array while they are higher than low and lower than or equal to high
-  while (low <= high) {
-    digitArray.push(low++);
+  // console.log(sorted);
+  let lowest = sorted[0];
+  console.log(lowest);
+  let highest = sorted[sorted.length-1];
+  console.log(highest);
+  let newArr = [];
+  while (lowest <= highest) {
+    newArr.push(lowest++);
   }
-  console.log(digitArray);
-  // get total of all numbers in array
-  let totalSum = digitArray.reduce(function(a, b) {
+  // console.log(newArr);
+  let arrSum = newArr.reduce(function(a, b) {
     return a + b;
   })
-  console.log(totalSum);
+  // console.log(arrSum);
+  return arrSum;
 }
 
-sumAll([5, 10]);
+sumAll([1, 4]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sumAll(arr) {
+//   // sort array in ascending order
+//   let sorted = arr.sort(function(a, b) {
+//     return a - b;
+//     return sorted;
+//   })
+//   // console.log(sorted
+//   // created variable for lowest number which is the first number in the array
+//   let low = sorted[0];
+//   console.log(low);
+//   // created variable for highest number which is the last number in array
+//   let high = sorted[sorted.length-1];
+//   console.log(high);
+//   // created new array to store all numbers
+//   let digitArray = [];
+//   // added numbers to new array while they are higher than low and lower than or equal to high
+//   while (low <= high) {
+//     digitArray.push(low++);
+//   }
+//   console.log(digitArray);
+//   // get total of all numbers in array
+//   let totalSum = digitArray.reduce(function(a, b) {
+//     return a + b;
+//   })
+//   console.log(totalSum);
+// }
+//
+// sumAll([5, 10]);
