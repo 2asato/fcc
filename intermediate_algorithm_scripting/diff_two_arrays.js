e3d0c76316ea2f29d2a31f8892985ff9ba662261
aaronsato <2asato@gmail.com>
2018-12-12 16:34:18 -0800

    100days redo

--- intermediate_algorithm_scripting/diff_two_arrays.js
+++ intermediate_algorithm_scripting/diff_two_arrays.js
@@ -1,4 +1,49 @@
 function diffArray(arr1, arr2) {
+  // Same, same; but different.
+  let newArr = arr1.concat(arr2);
+  console.log(newArr);
+  return newArr.filter(x => arr1.indexOf(x) === -1 || arr2.indexOf(x) === -1)
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function diffArray(arr1, arr2) {
 
   // Same, same; but different.
   // let newArr = arr1.concat(arr2);
