15874a8f0bd564a9de5931032b85b58d1d32edbe
aaronsato <2asato@gmail.com>
2018-12-14 14:13:24 -0800

    got my pig latin straight

--- intermediate_algorithm_scripting/pig_latin.js
+++ intermediate_algorithm_scripting/pig_latin.js
@@ -5,13 +5,20 @@ function translatePigLatin(str) {
   let letterArray = str.split("");
   if (str[0].match(regex)) {
     pigLatin = str + 'way';
+  } else if(str.match(regex) === null) {
+    // Check if the string contains only consonants
+    pigLatin = str + 'ay';
   } else {
-    firstConsonant = letterArray.shift();
-    // console.log(firstConsonant);
-    // console.log(letterArray);
-    pigLatin = letterArray.concat(firstConsonant + 'ay').join("");
-    console.log(pigLatin);
+
+    // Find how many consonants before the first vowel.
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+    // Take the string from the first vowel to the last char
+    // then add the consonants that were previously omitted and add the ending.
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + 'ay';
   }
+
+  return pigLatin;
 }
 
 translatePigLatin("consonant");
