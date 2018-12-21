9849260a9ce24cbc831775dae66be930561681d7
aaronsato <2asato@gmail.com>
2018-12-21 14:33:15 -0800

    file created and setup complete

---
+++ intermediate_algorithm_scripting/dna_pairing.js
@@ -0,0 +1,10 @@
+function pairElement(str) {
+  // turn str into arr
+  // loop thru arr
+  // if g return c, c return g, t return a, a return t
+  // put pair into own array
+  // put all arrays into one array
+  return str;
+}
+
+pairElement("GCG");
