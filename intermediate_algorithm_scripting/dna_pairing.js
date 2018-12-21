6046252b3b34e6d99c4971d01dc48dadbaff6c15
aaronsato <2asato@gmail.com>
2018-12-21 15:52:10 -0800

    split str into arr

--- intermediate_algorithm_scripting/dna_pairing.js
+++ intermediate_algorithm_scripting/dna_pairing.js
@@ -1,6 +1,17 @@
 function pairElement(str) {
   // turn str into arr
+  let strArr = str.split("");
+  let subArr = [];
+// console.log(strArr);
   // loop thru arr
+  for (let i = 0; i < strArr.length; i++) {
+    // console.log(strArr[i]);
+    console.log(strArr[i + 1]);
+    // if (strArr[i] === 'G') {
+    //   strArr.splice();
+    //   console.log(strArr);
+    // }
+  }
   // if g return c, c return g, t return a, a return t
   // put pair into own array
   // put all arrays into one array
