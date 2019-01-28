16b4eca226abe6d8de067049aa39ccb41cac279f
aaronsato <2asato@gmail.com>
2019-01-28 11:56:13 -0800

    100 days redo

--- basic_algorithm_scripting/factorialize_a_number.js
+++ basic_algorithm_scripting/factorialize_a_number.js
@@ -1,12 +1,9 @@
 function factorialize(num) {
-  if (num <= 1)
+  if (num === 0 || num === 1) {
     return 1;
-    let fact = factorialize(num - 1);
-    let result = num * fact;
-    return result;
-    console.log(result);
-
-}
+    }
+    return num * factorialize(num - 1)
+  }
 
 console.log(factorialize(10));
 
@@ -14,11 +11,11 @@ console.log(factorialize(10));
 
 
 
-function factorialize(num) {
-  if (num === 0 || num === 1){
-    return 1;
-    }
-    return num * factorialize(num - 1)
-  }
-
-console.log(factorialize(5));
+// function factorialize(num) {
+//   if (num === 0 || num === 1){
+//     return 1;
+//     }
+//     return num * factorialize(num - 1)
+//   }
+//
+// console.log(factorialize(5));
