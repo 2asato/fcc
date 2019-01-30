5386c4f60befb985c27db0b2e75fbe846be576eb
aaronsato <2asato@gmail.com>
2019-01-29 17:48:54 -0800

    100 days redo

--- basic_algorithm_scripting/largest_num_in_array.js
+++ basic_algorithm_scripting/largest_num_in_array.js
@@ -1,12 +1,34 @@
 function largestOfFour(arr) {
   // You can do this!
-  let maxSubArr = arr.map(subArr => Math.max(...subArr));
-  // let largest = []
-  // largest.push(Math.max(...maxSubArr));
-  console.log(maxSubArr);
+  let max = arr.map(subArr => Math.max(...subArr))
+  console.log(max);
+  // return arr;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function largestOfFour(arr) {
+//   // You can do this!
+//   let maxSubArr = arr.map(subArr => Math.max(...subArr));
+//   // let largest = []
+//   // largest.push(Math.max(...maxSubArr));
+//   console.log(maxSubArr);
+// }
+//
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
 
 
 
