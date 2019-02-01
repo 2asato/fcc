b15f471cd530ca2b6093067a6dc8b29976a827b4
aaronsato <2asato@gmail.com>
2019-02-01 11:51:37 -0800

    100 days redo

--- basic_algorithm_scripting/slice_and_spice.js
+++ basic_algorithm_scripting/slice_and_spice.js
@@ -1,18 +1,43 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
-  // create new array with contents of arr2
-  let sliced2 = arr2.slice();
-  // console.log(sliced2);
-  // add each element off arr1 into sliced2
+  let new2 = [...arr2];
+  console.log(new2);
   for (let i = 0; i < arr1.length; i++) {
-    sliced2.splice(n, 0, arr1[i])
-    // increases n, changing the index place of element being added
+    new2.splice(n, 0, arr1[i])
     n++;
-    // console.log(sliced2);
   }
+  console.log(new2);
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function frankenSplice(arr1, arr2, n) {
+//   // It's alive. It's alive!
+//   // create new array with contents of arr2
+//   let sliced2 = arr2.slice();
+//   // console.log(sliced2);
+//   // add each element off arr1 into sliced2
+//   for (let i = 0; i < arr1.length; i++) {
+//     sliced2.splice(n, 0, arr1[i])
+//     // increases n, changing the index place of element being added
+//     n++;
+//     // console.log(sliced2);
+//   }
+// }
+//
+// frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 
 
