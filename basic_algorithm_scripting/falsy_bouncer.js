bf16448db1298a11462c3bba70e17137d645d903
aaronsato <2asato@gmail.com>
2019-02-01 13:25:42 -0800

    100 days redo

--- basic_algorithm_scripting/falsy_bouncer.js
+++ basic_algorithm_scripting/falsy_bouncer.js
@@ -1,6 +1,7 @@
 function bouncer(arr) {
   // Don't show a false ID to this bouncer.
-  return arr.filter(Boolean);
+// return arr.filter(Boolean)
+console.log(arr.filter(Boolean));
 }
 
 bouncer([7, "ate", "", false, 9]);
