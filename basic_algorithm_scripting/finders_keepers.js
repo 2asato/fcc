e9942e586c83ffd6dbdce244717f240eebe6edd2
aaronsato <2asato@gmail.com>
2019-02-08 11:46:54 -0800

    100 days redo

--- basic_algorithm_scripting/finders_keepers.js
+++ basic_algorithm_scripting/finders_keepers.js
@@ -1,20 +1,43 @@
 function findElement(arr, func) {
-  let num;
   // return num;
   for (let i = 0; i < arr.length; i++) {
-    if (func(arr[i])) {
-      num = arr[i];
+    let num = arr[i];
+    if (func(num)) {
       return num;
     }
   }
-  return undefined;
-
-
 }
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
 
 
 
-console.log(findElement([1, 7, 3, 5], num => num % 2 === 0));
+
+
+
+
+
+
+
+
+
+
+// function findElement(arr, func) {
+//   let num;
+//   // return num;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (func(arr[i])) {
+//       num = arr[i];
+//       return num;
+//     }
+//   }
+//   return undefined;
+
+
+
+
+
+
+// console.log(findElement([1, 7, 3, 5], num => num % 2 === 0));
 
 
 
