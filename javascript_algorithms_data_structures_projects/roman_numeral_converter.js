ff81c8438a6af08ee34c84fe9483deec26034d17
aaronsato <2asato@gmail.com>
2019-02-13 09:26:50 -0800

    set up variables

--- javascript_algorithms_data_structures_projects/roman_numeral_converter.js
+++ javascript_algorithms_data_structures_projects/roman_numeral_converter.js
@@ -1,4 +1,7 @@
 function convertToRoman(num) {
+  let numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  let romanNums = [I, IV, V, IX, X, XL, L, XC, C, CD, D, CM, M];
+  let romaned = "";
  return num;
 }
 
