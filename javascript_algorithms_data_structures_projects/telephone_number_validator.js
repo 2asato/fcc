fe8e59b4715fc1f7b66095432704788a37b42a0e
aaronsato <2asato@gmail.com>
2019-02-20 08:46:43 -0800

    doesnt pass if starts with a 1 or if unwanted character mixed in

--- javascript_algorithms_data_structures_projects/telephone_number_validator.js
+++ javascript_algorithms_data_structures_projects/telephone_number_validator.js
@@ -1,11 +1,10 @@
 function telephoneCheck(str) {
   // Good luck!
-  let phoneNumber = //;
-  regex.test(str);
-  return true;
+  let phoneNumber = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+  console.log(phoneNumber.test(str));
 }
 
-telephoneCheck("555-555-5555");
+telephoneCheck("555 555 5555");
 
 // formats that pass
 // 555-555-5555
@@ -14,3 +13,7 @@ telephoneCheck("555-555-5555");
 // 555 555 5555
 // 5555555555
 // 1 555 555 5555
+
+
+// not passing if starts with a 1
+// not passing if theres unwanted character mixed in
