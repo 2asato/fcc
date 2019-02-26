e7f2e3595024ccb0142b76815e96d2ea822d3a94
aaronsato <2asato@gmail.com>
2019-02-25 20:50:34 -0800

    complete phone number validator

--- javascript_algorithms_data_structures_projects/telephone_number_validator.js
+++ javascript_algorithms_data_structures_projects/telephone_number_validator.js
@@ -1,10 +1,22 @@
 function telephoneCheck(str) {
   // Good luck!
-  let phoneNumber = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-  console.log(phoneNumber.test(str));
-}
+  let phoneNUm = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+     return phoneNUm.test(str);
+  }
+  telephoneCheck("555-555-5555");
 
-telephoneCheck("555 555 5555");
+// ^ denotes the beginning of the string.
+
+// (1\s?)? allows for “1” or “1 ” if there is one.
+
+// \d{n} checks for exactly n number of digits so \d{3} checks for three digits.
+
+// x|y checks for either x OR y so (\(\d{3}\)|\d{3}) checks for either three digits surrounded by parentheses, or three digits by themselves with no parentheses.
+
+// [\s\-]? checks for spaces or dashes between the groups of digits.
+
+// $ denotes the end of the string. In this case the beginning ^ and end of the string $ are used in the regex to prevent it from matching any longer string that might contain a valid phone number (eg. “s 555 555 5555 3”).
+
 
 // formats that pass
 // 555-555-5555
